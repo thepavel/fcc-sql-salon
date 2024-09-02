deef088f352f8b1d790209720e0ae9dcc4c6b5fa
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-02 05:52:48 +0000

    wip: playing around

--- salon.sh
+++ salon.sh
@@ -3,38 +3,52 @@
 PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
 
-MAIN_MENU() {
-  echo -e "Welcome to My Salon, how can I help you?\n"
-
+PRINT_SERVICES() {
   # get services
   SERVICES_RESULT=$($PSQL "SELECT service_id, name FROM services")
-  
   # display services
+  echo -e "\n$1"
   echo "$SERVICES_RESULT" | sed 's/|/) /' 
-  SERVICES_IDS=($(echo "$SERVICES_RESULT | sed 's/^([0-9]+).*/\1/'"))
-  echo $SERVICES_IDS
+}
+
+GET_SERVICE_SELECTION() {
+  PRINT_SERVICES
   # read input
   read SERVICE_ID_SELECTED
 
-  # if not valid
-  if ! [[ $SERVICE_ID_SELECTED ~= '^[0-9]+$' ]]
+  if [[ $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]] # it's a number - basic validation
   then 
-    echo -e "\nI could not find that service. What would you like today?"
-    # display services again
-  else 
-    # if not available
+    SERVICE_RESULT=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+    echo $SERVICE_RESULT
+
+    if [[ -z $SERVICE_RESULT ]]
+    then
+      echo -e "\nI could not find that service. What would you like today?"  
+    else 
+      echo -e "\nWhat's your phone number?"
+      read CUSTOMER_PHONE
+    fi
     
-    # echo -e "\nI could not find that service. What would you like today?"  
-    # display services again
-    # if valid collect phone number
-    #  get name from db if exists 
-    # or create customer after prompting for name
+  else 
+    echo -e "\nI could not find that service. What would you like today?"
   fi
-  
-  
-  
-  
+
+  # # if not valid
+  # if [[ $SERVICE_ID_SELECTED =~ '^[0-9]+$' ]]
+  # then 
+    
+  #   # display services again
+  #   # if not available
+  #   # echo -e "\nI could not find that service. What would you like today?"  
+  #   # display services again
+  #   # if valid collect phone number
+  #   #  get name from db if exists 
+  #   # or create customer after prompting for name
+  # fi
+
 }
 
-MAIN_MENU 
+
+  GET_SERVICE_SELECTION 
